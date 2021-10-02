23e8e69bd953f55f93b14305122462510afe58ca
Radan Elena <arion_ellena@yahoo.com>
2021-10-02 18:27:22 +0300

    "rezolvare ex15"

--- session2/ex15.py
+++ session2/ex15.py
@@ -6,3 +6,12 @@
         Veti primi: 'cmi'
         Veti printa: 1
 """
+x = input("Introduceti un cuvant: ")
+
+l1 = []
+
+for i in x:
+    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+        l1.append(i)
+    
+print("Cuvantul introdus are {} vocale".format(len(l1)))
