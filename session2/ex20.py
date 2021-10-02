0f42394d370ea2a6379efdbef93523c31f6ec37b
Radan Elena <arion_ellena@yahoo.com>
2021-10-02 19:59:36 +0300

    "rezolvare ex20"

--- session2/ex20.py
+++ session2/ex20.py
@@ -16,3 +16,35 @@
             2: 'i'
         }
 """
+
+user = input("Introduceti un string: ")
+x = int(input("Numarul introdus trebuie sa fie egal cu lungimea stringului: "))
+
+"""
+    Pentru a ma asigura ca numarul introdus de la tastatura este egal cu lungimea stringului
+    am folosit un while
+"""
+while x != len(user):
+    print("Numarul introdus de dumneavoastra are o valoare diferita de cea a lungimii stringului!")
+    x = int(input("Introduceti un numar egal cu lungimea stringului: "))
+
+l1 = []           # am creat o lista in care voi adauga cu ajutorul unui 'for' toate literele din string
+l2 = []           # am creat o lista in care voi adauga cu ajutorul unui 'for' toate cifrele de la 0 la x
+
+for i in user:
+    l1.append(i)
+
+for i in range(x):
+    l2.append(i)
+
+# Am creat dictionarul 
+
+d1 = {}
+
+# Cu ajutorul lui 'for' for introduce valori in dictionar
+
+for i in range(x):
+    d1[l2[i]]=l1[i]
+
+print(d1)
+
