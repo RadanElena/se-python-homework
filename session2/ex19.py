30d415cd5fec654e7ab83cec27aff56c6350adb3
Radan Elena <arion_ellena@yahoo.com>
2021-10-02 19:06:48 +0300

    "rezolvare ex19"

--- session2/ex19.py
+++ session2/ex19.py
@@ -6,3 +6,21 @@
         Veti primi: 'cmi'
         Veti printa: ('c', 'm', 'i')
 """
+
+user = input("Introduceti un string: ")
+
+"""
+    Deoarece nu putem adauga direct valori intr-un tuple (odata creat, nu ii poti adauga ulterior valori),
+     mai intai am creat o lista pentru a introduce literele stringului
+"""
+list = []
+
+for i in user:
+    list.append(i)
+
+# Am convertit lista int-un tuple
+
+list_converted_in_tuple = tuple(list)
+
+# Am afisat tuple-ul
+print(list_converted_in_tuple)
