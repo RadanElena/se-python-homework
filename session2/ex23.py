486572f7200bcf0145711b8043031415a0175ed7
Radan Elena <arion_ellena@yahoo.com>
2021-10-02 21:20:05 +0300

    "rezolvare ex23"

--- session2/ex23.py
+++ session2/ex23.py
@@ -14,3 +14,11 @@
         Veti primi: 'cojoc'
         Veti printa: True
 """
+# Am creat o variabila 'user' a carui valoare ii este atribuita de la tastatura
+user = input("Introduceti un string: ")
+
+# Am creat un "if" ce verifica daca stringul introdus este palindrom
+if user == user[::-1]:        # user[::-1] intoarce in oglinda cuvantul (de exemplu "gol" devine "log")
+    print("True")
+else:
+    print("False")
