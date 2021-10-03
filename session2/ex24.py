9072589d1092512cc2288cb76b5ab1f265ec5d5e
Radan Elena <arion_ellena@yahoo.com>
2021-10-03 10:31:26 +0300

    "rezolvare ex24"

--- session2/ex24.py
+++ session2/ex24.py
@@ -14,3 +14,23 @@
         Veti primi: 1232
         Veti printa: False
 """
+
+# Am creat o variabila 'user' a carui valoare ii este atribuita de la tastatura
+user = input("Introduceti un numar intreg: ")        # am lasat valoarea sa fie de tip string pentru a fi mai usor de inversat
+
+# Am creat o variabila n1 a carui valoare este valoarea in oglinda a variabilei user (ex: user = '1232' atunci n1 = '2321')
+n1 = user[::-1]
+
+# Am transformat variabilele, din unele de tip string in unele de tip integer
+
+user = int(user)
+n1 = int(n1)
+
+"""
+    Am creat un if care verifica daca cele doua variabile sunt egale sau nu, 
+    daca cele doua variabile sunt egale, inseamna ca variabila user este un numar palindrom 
+"""
+if user == n1:
+    print(True)
+else:
+    print(False)
