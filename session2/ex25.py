2039f05142eab405340d6c26d33d0b03f1863d66
Radan Elena <arion_ellena@yahoo.com>
2021-10-03 10:54:42 +0300

    "rezolvare ex25"

--- session2/ex25.py
+++ session2/ex25.py
@@ -9,3 +9,34 @@
         Veti printa prima data: [1, 3, 4, 5, 5]
         Veti prina a doua oara: {1, 3, 4, 5}
 """
+
+# Am afisat conditia pentru care loop-ul se opreste (l-am pus primul doar pentru ca mi se parea ca asa arata mai bine la ouput :D)
+print("Pentru a va opri, tastati 'exit'")
+
+""" 
+    Am creat o variabila 'user' a carui valoare ii este atribuita de la tastatura, si am lasat-o 
+    initial de tip string pentru a se putea verifica usor conditia din 'while'
+"""
+user = input("Introduceti un numar: ")
+
+# Am creat o lista goala, in care voi adauga ulterior valori cu ajutorul lui 'while'
+l1 = []
+
+"""
+    Am creat un loop de tip 'while' a carui conditie sa se opreasca e sa se introduca 
+    cuvantul 'exit' de la tastatura, cat timp acesta ruleaza, va adauga pe rand in lista 
+    'l1' toate numerele introduse de la tastatura
+"""
+
+while user != "exit":
+    l1.append(int(user))                           # am transformat variabila de tip string in int si am introduso in l1
+    user = input("Introduceti un numar: ")
+
+# Am afisat lista 'l1'
+print(l1)
+
+# Am creat un set din lista l1
+l2 = set(l1)
+
+# Am afisat valorile setului l2
+print(l2)
