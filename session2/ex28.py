8545e6087bd60f7e607178ceffa4f4aaf5fe3723
Radan Elena <arion_ellena@yahoo.com>
2021-10-03 12:29:06 +0300

    "rezolvare ex28"

--- session2/ex28.py
+++ session2/ex28.py
@@ -6,3 +6,18 @@
         Veti primi: 5
         Veti printa: 15
 """
+# Am creat variabila 'user' a carui valori ii sunt atribuite de la tastatura
+user = int(input("\nIntroduceti un numar: "))
+
+# Am creat o variabila de tip int, caruia i-am atribuit initial valoarea zero
+suma = 0
+
+"""
+    Am creat un 'for' care va face practic suma numerelor pana la numarul introdus 
+    de la tastatura, iar valoarea o va atribui variabilei 'suma'
+"""
+for i in range(0, user+1):
+    suma = suma + i
+
+# Am afisat suma numerelor
+print("Suma numerelor de la 0 la {} este: {}".format(user, suma))
